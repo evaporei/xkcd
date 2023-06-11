0d0cf2fb9332cf4d12ad21c492d4bf9a0cb395ef
Eva Pace <eba.pachi@gmail.com>
2023-06-11 18:05:05 -0300

    fetch last comic info

--- cmd/xkcd_fetch/main.go
+++ cmd/xkcd_fetch/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "net/http"
+)
+
+type ComicInfo struct {
+  Num int `json:"num"`
+	SafeTitle string `json:"safe_title"`
+	Transcript string `json:"transcript"`
+	Title string `json:"title"`
+}
 
 func main() {
-  fmt.Println("xkcd-fetch")
+  lastComicUrl := "https://xkcd.com/info.0.json"
+
+  res, err := http.Get(lastComicUrl)
+  if err != nil {
+    fmt.Printf("xkcd_fetch: failed to get last xkcd comic: %s\n", err)
+    os.Exit(1)
+  }
+
+  if res.Body != nil {
+    defer res.Body.Close()
+  }
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Printf("xkcd_fetch: failed to read the body of the last xkcd comic: %s\n", err)
+    os.Exit(1)
+  }
+
+  lastComicInfo := ComicInfo{}
+  err = json.Unmarshal(body, &lastComicInfo)
+  if err != nil {
+    fmt.Printf("xkcd_fetch: failed to unmarshal the JSON body of the last xkcd comic: %s\n", err)
+    os.Exit(1)
+  }
+
+  fmt.Println("last comic number:", lastComicInfo.Num)
 }
